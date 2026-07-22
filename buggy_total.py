2c197172f26d299cd406a6a27a7ec3f2e205ad94
Paul_UTSA <144975006+paulutsa@users.noreply.github.com>
2026-07-22 15:19:25 -0500

    Add files via upload

---
+++ buggy_total.py
@@ -0,0 +1,9 @@
+def calculate_total(price, quantity):
+    total = price * quantity
+    return totla
+
+price = 5
+quantity = 3
+
+if quantity = 3:
+    Print("The total is:", calculate_total(price, quantity))
